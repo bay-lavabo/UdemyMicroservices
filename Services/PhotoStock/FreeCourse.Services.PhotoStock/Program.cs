84807c8c78ec9c3758c44201deda943f4eade897
bay-lavabo <mrorhanfirtina@gmail.com>
2023-07-15 15:36:54 +0300

    PhotoStockAPI eklendi.

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new AuthorizeFilter());
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = configuration["IdentityServerURL"];
+    options.Audience = "resource_photo_stock";
+    options.RequireHttpsMetadata = false;
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseStaticFiles();
+app.UseAuthorization();
+app.UseAuthentication();
+app.MapControllers();
+
+app.Run();
